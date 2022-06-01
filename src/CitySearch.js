21cfdab9fd575e17de4aad9716120b9674a77fb7
ironmike84 <michaelgross.tn@gmail.com>
2022-06-01 07:56:39 -0400

    New

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -5,14 +5,27 @@ class CitySearch extends Component {
     super();
  
     this.state = {
-      query: ''
+      query: '',
+      suggestions: []
     }
   }
-
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
   }
+
   render() {
     return (
       <div className="CitySearch">
@@ -22,8 +35,17 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.handleInputChanged}
         />
-        <ul className="suggestions">
-        </ul>
+       <ul className="suggestions">
+  {this.state.suggestions.map((suggestion) => (
+    <li
+      key={suggestion}
+      onClick={() => this.handleItemClicked(suggestion)}
+    >{suggestion}</li>
+  ))}
+  <li>
+    <b>See all cities</b>
+  </li>
+</ul>
       </div>
     );
   }
