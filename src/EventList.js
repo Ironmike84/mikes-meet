2626b03df9f5d727c2f4fb8b40c319e3fdab4d26
ironmike84 <michaelgross.tn@gmail.com>
2022-06-15 14:26:41 -0400

    update

--- src/EventList.js
+++ src/EventList.js
@@ -1,21 +1,10 @@
+import React, { Component } from 'react'
 
-import React, { Component } from 'react';
-import Event from './Event'
-
-
-class EventList extends Component {
-  render() {
-    const { events } = this.props;
-    return (
-      <ul className="EventList">
-        {events.map(event =>
-         <li key={event.id}>
-         <Event event={event} />
-       </li>
-        )}
-      </ul>
-    );
-  }
+export default class EventList extends Component {
+    render() {
+        return (
+          <ul className="EventList">
+          </ul>
+        );
+      }
 }
-
-export default EventList;
--- src/__Tests__/App.test.js
+++ src/__Tests__/App.test.js
@@ -1,28 +1,15 @@
+// src/__tests__/App.test.js
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList'
-import CitySearch from '../CitySearch'
 
 describe('<App /> component', () => {
-  let AppWrapper;
-  beforeAll(() => {
-    AppWrapper = shallow(<App />);
-  });
-
   test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
-
-  test('render CitySearch', () => {
-    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-  });
-  test('renders a list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
-    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
-  });
 });
-
 
 
