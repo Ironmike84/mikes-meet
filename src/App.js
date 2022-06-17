a82181b6c915e5df32ad2d10c1619c2b81a4cc9e
ironmike84 <michaelgross.tn@gmail.com>
2022-06-17 13:25:42 -0400

    New

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import { mockData } from './mock-data'
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
 
 class App extends Component {
   state = {
-    events: []
+    events: [],
+    locations: [],
   }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList events={mockData}/>
+        <CitySearch locations={this.state.locations}  updateEvents={this.updateEvents}  />
+        <EventList events={this.state.events}/>
         <NumberOfEvents/>
         </div>
     );
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -27,6 +27,8 @@ class CitySearch extends Component {
     this.setState({
       query: suggestion
     });
+  
+    this.props.updateEvents(suggestion);
   }
 
   render() {
