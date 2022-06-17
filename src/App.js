822ea2eda307eb57243f41c05e36909b427bff12
ironmike84 <michaelgross.tn@gmail.com>
2022-06-17 05:04:24 -0400

    Yes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import { mockData } from './mock-data'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />
-        <EventList />
-      </div>
+        <EventList events={mockData}/>
+        </div>
     );
   }
 }
