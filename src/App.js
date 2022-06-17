926b4c8a4a31e19dadd9f620c443cffaa7676b9a
ironmike84 <michaelgross.tn@gmail.com>
2022-06-17 06:04:00 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import { mockData } from './mock-data'
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+  state = {
+    events: mockData
+  }
   render() {
     return (
       <div className="App">
         <CitySearch />
-        <EventList events={mockData}/>
+        <EventList events={this.state.events}/>
         <NumberOfEvents/>
         </div>
     );
--- src/__Tests__/App.test.js
+++ src/__Tests__/App.test.js
@@ -1,7 +1,7 @@
 // src/__tests__/App.test.js
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
@@ -27,4 +27,14 @@ beforeAll(() => {
     test('render NumberOfEvents', () => {
       expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
     });
+});
+
+describe('<App /> integration', () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  });
 });
