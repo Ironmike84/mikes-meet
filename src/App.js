57d9f39f6e055cc292ff70ff3040e7cbb0d67c58
ironmike84 <michaelgross.tn@gmail.com>
2022-06-17 05:48:12 -0400

    God is good

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import { mockData } from './mock-data'
 
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch />
-        <EventList/>
+        <EventList events={mockData}/>
         </div>
     );
   }
