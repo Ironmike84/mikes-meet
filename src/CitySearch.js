b9ef4d073ae404d35e9ee182c943f91e128751cc
ironmike84 <michaelgross.tn@gmail.com>
2022-06-17 06:38:19 -0400

    Good

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -15,13 +15,14 @@ class CitySearch extends Component {
   handleInputChanged = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({
-      query: value,
-      suggestions,
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+      });
+    this.setState({ 
+        query: value,
+        suggestions, 
     });
   };
+
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion
