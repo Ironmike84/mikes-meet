bf79658641ff7ad540063a8a5ff37d92d358cf69
ironmike84 <michaelgross.tn@gmail.com>
2022-06-19 08:22:32 -0400

    Changed

--- src/api.js
+++ src/api.js
@@ -30,7 +30,7 @@ const getToken = async (code) => {
   try {
       const encodeCode = encodeURIComponent(code);
 
-      const response = await fetch( 'https://yvylbz2ov2.execute-api.us-east-1.amazonaws.com/dev/api/token/{code}' + '/' + encodeCode);
+      const response = await fetch( 'https://yvylbz2ov2.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
       if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`)
       }
@@ -55,7 +55,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://yvylbz2ov2.execute-api.us-east-1.amazonaws.com/dev/api/get-events/{access_token}' + '/' + token;
+    const url = 'https://yvylbz2ov2.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
